5c32ce178c033d5a83462bd6f290a40e53e71f31
jordanvtskier12 <42844401+jordanvtskier12@users.noreply.github.com>
2018-10-30 13:05:18 -0400

    Updated from Brython Server: 10/30/2018 1:05:14 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,8 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+jumb=input("Please enter a string of text (the bigger the better): ")
+
+print('You entered '  + jumb + '. Now jumble it: ')
+
+print(jumb[::-1])
